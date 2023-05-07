28a795e44bd9e20392b60efc359d4478a30ef10a
mada145 <52374808+mada145@users.noreply.github.com>
2023-05-07 12:30:42 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,54 +1,55 @@
+var video1 = new Image();
+video1.src = "video1.mp4";
 
-var video = document.getElementById("video");
+var video2 = new Image();
+video2.src = "video2.mp4";
+
 var currentVideo = "video1.mp4";
 var switchTimer = null;
 
-// preload videos in the background
-var preloadVideo1 = new Image();
-preloadVideo1.src = "video1.mp4";
-var preloadVideo2 = new Image();
-preloadVideo2.src = "video2.mp4";
-
 function startSwitchTimer() {
-    switchTimer = setInterval(function() {
-        switchVideo();
-    }, 1000); // set the timer threshold to 1 second
+  switchTimer = setTimeout(function () {
+    switchVideo();
+  }, 1000); // set the timer threshold to 1 second
 }
 
 function stopSwitchTimer() {
-    if (switchTimer !== null) {
-        clearInterval(switchTimer);
-        switchTimer = null;
-    }
+  if (switchTimer !== null) {
+    clearTimeout(switchTimer);
+    switchTimer = null;
+  }
 }
 
 function switchVideo() {
-    if (currentVideo == "video1.mp4") {
-        currentVideo = "video2.mp4";
-        video.src = currentVideo;
-        video.play();
-    } else {
-        currentVideo = "video1.mp4";
-        video.src = currentVideo;
-        video.play();
-    }
+  if (currentVideo === "video1.mp4") {
+    currentVideo = "video2.mp4";
+  } else {
+    currentVideo = "video1.mp4";
+  }
+  video1.currentTime = video2.currentTime;
+  video2.currentTime = video1.currentTime;
+  var videoElement = document.getElementById("video");
+  videoElement.src = currentVideo;
+  videoElement.play();
 }
 
-document.addEventListener("keydown", function(event) {
-    if (event.key === "t") {
-        event.preventDefault();
-        switchVideo();
-    }
+document.addEventListener("keydown", function (event) {
+  if (event.key === "t") {
+    event.preventDefault();
+    switchVideo();
+  }
 });
 
 var container = document.getElementById("container");
-container.addEventListener("touchstart", function(event) {
-    startSwitchTimer();
+container.addEventListener("touchstart", function (event) {
+  event.preventDefault();
+  startSwitchTimer();
 });
 
-container.addEventListener("touchend", function(event) {
-    stopSwitchTimer();
-    if (switchTimer === null) {
-        switchVideo();
-    }
+container.addEventListener("touchend", function (event) {
+  event.preventDefault();
+  stopSwitchTimer();
+  if (switchTimer === null) {
+    switchVideo();
+  }
 });
